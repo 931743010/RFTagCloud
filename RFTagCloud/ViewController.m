58605b8287d85d20b0cfef0ecfeeb8b4ac456457
arvin <rorogestudio@gmail.com>
2015-11-18 16:41:56 +0800

    add class

--- RFTagCloud/ViewController.m
+++ RFTagCloud/ViewController.m
@@ -22,9 +22,9 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSArray *colorArray=@[RGB(51, 51, 51),RGB(102, 102, 102),RGB(102, 153, 0),RGB(0, 153, 255),RGB(255, 102, 0),RGB(255, 51, 51)];
-    NSArray *words = @[@"北京",@"火影忍者",@"足球小子",@"超人",@"葫芦娃大战蜘蛛精",@"super man",@"welcome to china",@"上海",@"海贼王",@"会说话的汤姆猫",@"大头儿子小头爸爸",@"少林足球",@"妖精的尾巴",@"咸蛋超人",@"蜘蛛侠",@"葫芦娃"];
+    NSArray *words = @[@"北京",@"火影忍者",@"足球小子",@"超人",@"葫芦娃大战蜘蛛精",@"super man",@"welcome to china",@"上海",@"海贼王",@"会说话的汤姆猫",@"大头儿子小头爸爸",@"少林足球",@"妖精的尾巴",@"咸蛋超人",@"蜘蛛侠",@"葫芦娃",@"北京",@"火影忍者",@"足球小子",@"超人",@"葫芦娃大战蜘蛛精",@"super man",];
     
-    RFTagCloudView *tagView = [[RFTagCloudView alloc] initWithFrame:CGRectMake(30, 100, 260, 300)];
+    RFTagCloudView *tagView = [[RFTagCloudView alloc] initWithFrame:CGRectMake(10, 100, 300, 300)];
     [tagView drawCloudWithWords:words colors:colorArray rowHeight:30];
     
     [self.view addSubview:tagView];
