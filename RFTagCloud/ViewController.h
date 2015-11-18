6e420167edd00765b60d68e8719e07658df74898
arvin <rorogestudio@gmail.com>
2015-11-18 15:10:35 +0800

    Initial Commit

---
+++ RFTagCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RFTagCloud
+//
+//  Created by Arvin on 15/11/18.
+//  Copyright © 2015年 mobi.refine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RFTagCloud/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RFTagCloud
+//
+//  Created by Arvin on 15/11/18.
+//  Copyright © 2015年 mobi.refine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
